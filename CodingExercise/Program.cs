2603a83871c0b7e26c62dc944a46013901eabb60
sumanmondal11 <mithumondal546@gmail.com>
2023-08-08 17:54:35 -0400

    Initial commit

---
+++ CodingExercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodingExercise
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
